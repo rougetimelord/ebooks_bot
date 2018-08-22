d8ed607381463bd36e5b8ad921d8eadb109b1282
Rouge <code@r0uge.org>
2018-08-21 21:32:59 -0700

    Fix stuff

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var url_s = window.location.search.substring(1);
+var url_s = window.location;
 var url = new URL(url_s);
 var key = url.searchParams.get('oauth_token')
 document.addEventListener('DOMContentLoaded', ()=>{document.getElementById('key').innerText=key})
