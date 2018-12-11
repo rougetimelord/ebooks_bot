6437304addb3eadbc87250e5f7a62906f8dc8345
Rouge <code@r0uge.org>
2018-12-10 18:16:58 -0800

    Add mentions initialization
    Bot used to reply to all mentions upon first startup with a new data.json.

--- bot.py
+++ bot.py
@@ -214,6 +214,9 @@ class Bot():
             print("Found %s mentions!" % len(mentions))
         else:
             print("No mentions uwu")
+        if self.last_id == 1:
+            self.last_id = mentions[-1].id
+            return
         for tweet in mentions:
             if self.last_id < tweet.id:
                 self.last_reply = tweet.id
